e977aa1101e50a4f7fb568731206ea4325a22a33
Estb <estevaoaugusto@hotmail.com>
2018-03-10 00:58:57 -0300

    Add files via upload

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -159,7 +159,7 @@ void ProcessMessageMasternode(CNode* pfrom, std::string& strCommand, CDataStream
 
         CValidationState state;
         CTransaction tx = CTransaction();
-        CTxOut vout = CTxOut(4999*COIN, darkSendPool.collateralPubKey);
+        CTxOut vout = CTxOut(999*COIN, darkSendPool.collateralPubKey);
         tx.vin.push_back(vin);
         tx.vout.push_back(vout);
         //if(AcceptableInputs(mempool, state, tx)){
@@ -602,7 +602,7 @@ void CMasterNode::Check()
     if(!unitTest){
         CValidationState state;
         CTransaction tx = CTransaction();
-        CTxOut vout = CTxOut(4999*COIN, darkSendPool.collateralPubKey);
+        CTxOut vout = CTxOut(999*COIN, darkSendPool.collateralPubKey);
         tx.vin.push_back(vin);
         tx.vout.push_back(vout);
 
