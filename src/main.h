47b6d8e2857458110e4f0e0c632b9a5d5bfe5576
Estb <estevaoaugusto@hotmail.com>
2018-03-11 16:08:12 -0300

    Add files via upload

--- src/main.h
+++ src/main.h
@@ -107,7 +107,7 @@ inline int64_t FutureDrift(int64_t nTime) { return nTime + 10 * 60; } // up to 1
 
 //inline unsigned int GetTargetSpacing(int nHeight) { return IsProtocolV2(nHeight) ? 60 : 60; }
 
-inline int64_t GetMNCollateral() { return 5000; }
+inline int64_t GetMNCollateral() { return 1000; }
 
 extern CScript COINBASE_FLAGS;
 extern CCriticalSection cs_main;
